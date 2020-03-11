58f7d3f5dbad9cc55eaa7743aed56022d53ef018
Anton Ageev <antforwork@mail.ru>
2020-03-11 22:37:02 +0300

    Added: - landscape layout for main_activity.xml - ru localization for strings.xml Failed (not inplemented): - TabLayout listeners set, - ViewPager integration with TabLayout

--- app/src/main/java/com/geekbrains/helloactivity/MainActivity.java
+++ app/src/main/java/com/geekbrains/helloactivity/MainActivity.java
@@ -4,12 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout mainTab;
+    private TextView contentTextView;
+    private TabItem tabToday;
+    private TabItem tabTom;
+    private TabItem tabTomTom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        // setContentDependingOnMainTab();
+
     }
+
+    private void initViews(){
+        contentTextView = findViewById(R.id.textContent);
+        mainTab = findViewById(R.id.mainTab);
+        tabToday = findViewById(R.id.tabToday);
+        tabTom = findViewById(R.id.tabTom);
+        tabTomTom = findViewById(R.id.tabTomTom);
+
+    }
+
+    // пока не разобрался, как повесить CLickListener на TabItem - выдает NullPointerException:
+    //  Caused by: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.view.View.setOnClickListener(android.view.View$OnClickListener)' on a null object reference
+    //        at com.geekbrains.helloactivity.MainActivity.setContentDependingOnMainTab(MainActivity.java:40)
+    /*
+    private void setContentDependingOnMainTab(){
+        tabToday.setOnClickListener(new CompoundButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.isSelected()) contentTextView.setText(R.string.stringContent);
+            }
+        });
+        tabTom.setOnClickListener(new CompoundButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.isSelected()) contentTextView.setText(R.string.stringContentTom);
+            }
+        });
+        tabTomTom.setOnClickListener(new CompoundButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.isSelected()) contentTextView.setText(R.string.stringContentTomTom);
+            }
+        });
+
+    }
+     */
 }
